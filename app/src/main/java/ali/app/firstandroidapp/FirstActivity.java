e0e714fee1b46e472b0ddefbc3fc265d8b826180
aliabb <ali3abbasov@gmail.com>
2021-09-14 23:55:32 +0300

    add: Send button backend function

--- app/src/main/java/ali/app/firstandroidapp/FirstActivity.java
+++ app/src/main/java/ali/app/firstandroidapp/FirstActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
 
     private Button myButton;
     private TextView myTextView;
     private Button mySecondActivityButton;
+    private Button sendButton;
     private Context context = this;
+
+    List<ListItem> things = new ArrayList<>();
 
     private int count = 0;
 
@@ -26,11 +35,13 @@ public class FirstActivity extends AppCompatActivity {
 
         myButton = (Button) findViewById(R.id.button);
         mySecondActivityButton = (Button) findViewById(R.id.secondActivityButton);
+        sendButton = (Button) findViewById(R.id.sendBtn);
         myTextView = (TextView) findViewById(R.id.textView);
 
         myButton.setOnClickListener(myButtonClick);
         mySecondActivityButton.setOnClickListener(startSecondActivity);
         mySecondActivityButton.setOnLongClickListener(startSecondActivityLong);
+        sendButton.setOnClickListener(sendListItemListener);
     }
 
     public void runSecondActivity(boolean flag) {
@@ -62,6 +73,22 @@ public class FirstActivity extends AppCompatActivity {
             runSecondActivity(false);
             //myTextView.append("");
             return true;
+        }
+    };
+
+    View.OnClickListener sendListItemListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.baseline_3d_rotation_black_48dp);
+            things.add(new ListItem("First title", bitmap, "First description"));
+            things.add(new ListItem("Second title", bitmap, "Second description"));
+            Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+            if (things.size() != 0) {
+                intent.putParcelableArrayListExtra("things", (ArrayList<? extends Parcelable>) things);
+                startActivity(intent);
+            }
+
+
         }
     };
 
